dffb7760ad9cf202f32e3b827c0d7a0ac7ab6e72
Hristo <hristo.meshinski@gmail.com>
2016-11-22 14:40:35 +0000

    figured out how to pull images for items and champions

--- src/myapplication/testing.py
+++ src/myapplication/testing.py
@@ -28,7 +28,7 @@ riotapi.set_api_key('23da5b32-c763-41ed-8d7a-f2b1023d8174')
 riotapi.set_load_policy(LoadPolicy.lazy)
 
 
-class summoners(db.Model):
+class Summoners(db.Model):
     id = db.Column(db.Integer, primary_key=True, default=lambda: uuid.uuid4().hex)
     summonerName = db.Column(db.Unicode(50), unique=True)
     summonerLevel = db.Column(db.Integer)
@@ -44,6 +44,27 @@ class summoners(db.Model):
 
     def __repr__(self):
         return ' %s' % self.summonerName
+
+        # class RunePages(db.Model):
+        #     id = db.Column(db.Integer, primary_key=True, default=lambda: uuid.uuid4().hex)
+        #     runePageName = db.Column(db.Unicode(50))
+        #     runePageCurrent = db.Column(db.BOOLEAN)
+        #     runePageRunes = db.Column(db.Unicode(500))
+        #     summoner_ID = db.Column(db.Integer, db.ForeignKey('summoner.id'))
+        #     foreign_keys =[Summoners.id]
+        # summonerID = db.Column(db.Integer, unique=True)
+
+        # def __init__(self, id, summonerName, summonerID):
+        # def __init__(self, id, runePageName, runePageCurrent, runePageRunes, summonerID):
+        #     self.id = id
+        #     self.runePageName = runePageName
+        #     self.runePageCurrent = runePageCurrent
+        #     self.runePageRunes = runePageRunes
+        #     self.summonerID = summonerID
+        #     # self.summonerID = summonerID
+        #
+        # def __repr__(self):
+        #     return ' %s' % self.runePageName
 
 
 db.drop_all()
@@ -64,44 +85,72 @@ def init(app):
         app.permanent_session_lifetime = timedelta(seconds=60)
 
     except:
-        print ('Could not read config: '), config_location
+        print ('Could not read  config: '), config_location
+
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    err = None
+
+    if request.method == 'POST':
+        if not request.form['summonerName']:
+            err = 'Please provide your summoner name'
+        # elif not request.form['region']:
+        #     err = 'Please set your region'
+        else:
+            summonerName = request.form['summonerName']
+            return redirect(url_for('summoner', sumName=summonerName))
+            # return redirect(url_for('summoner', sumName=summonerName,
+            #                         region=riotapi.set_region(request.form['region'])))
+
+    return render_template('homepage.html', err=err)
 
 
 @app.route('/summoner/<sumName>')
 def summoner(sumName):
-    exists = db.session.query(summoners.id).filter(
-        summoners.summonerName.ilike(sumName)).scalar() is not None
+    # riotapi.set_region(region)
+    exists = db.session.query(Summoners.id).filter(
+        Summoners.summonerName.ilike(sumName)).scalar() is not None
     alreadyThere = ''
     summonerData = []
+    username = riotapi.get_summoner_by_name(sumName)
     if exists is False:
-        username = riotapi.get_summoner_by_name(sumName)
-        addNewSumomner = summoners(id=username.id, summonerName=username.name,
+
+        addNewSumomner = Summoners(id=username.id, summonerName=username.name,
                                    summonerLevel=username.level)
-
-        rune_pages = riotapi.get_rune_pages(username)
-
-        app.logger.info(summoners.id)
 
         db.session.add(addNewSumomner)
         db.session.commit()
-        app.logger.info(summoners.id)  # increment the primary id
+
+        rune_pages = riotapi.get_rune_pages(username)
+        #
+        # addRunePages = RunePages(id=rune_pages.id, runePageName=rune_pages.name,
+        #                          runePageCurrent=rune_pages.current,
+        #                          runePageRunes=rune_pages.rune, summonerID=username.id)
+        #
+        # db.session.add(addRunePages)
+        # db.session.commit()
 
         # covert the results from the query into a dict with column names and value pairs
-        query = summoners.query.filter_by(id=username.id).first()
+        query = Summoners.query.filter_by(id=username.id).first()
 
         dictionary = dict((col, getattr(query, col))
                           for col in
                           query.__table__.columns.keys())
 
+        match_list = username.match_list()
+        match = match_list[0].match()
+
         return render_template('testhome.html', summoner=username,
                                alreadyThere=alreadyThere,
                                rune_pages=rune_pages,
-                               summonerData=dictionary)
+                               summonerData=dictionary,
+                               match_info=match)
 
     else:
         alreadyThere = 'They are there'
 
-        query = summoners.query.filter(summoners.summonerName.contains(sumName)).first()
+        query = Summoners.query.filter(Summoners.summonerName.contains(sumName)).first()
 
         dictionary = dict((col, getattr(query, col))
                           for col in
@@ -109,11 +158,15 @@ def summoner(sumName):
 
         summonerData = dict((row.summonerLevel, row)
                             for row in
-                            db.session.query(summoners).filter(
-                                summoners.summonerName.ilike(sumName)
+                            db.session.query(Summoners).filter(
+                                Summoners.summonerName.ilike(sumName)
                             ))
+
+        rune_pages = riotapi.get_rune_pages(username)
         return render_template('testhomeSummoner.html', alreadyThere=alreadyThere,
-                               summonerData=dictionary)
+                               summonerData=dictionary,
+                               rune_pages=rune_pages
+                               )
 
 
 if __name__ == '__main__':
