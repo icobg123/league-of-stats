437200142ca0de1a7c501b5a5fd183274aa1959b
Hristo <hristo.meshinski@gmail.com>
2016-11-29 11:16:36 +0000

    Frodo - Its done. Sam - Yes, mr Frodo its over now https://www.youtube.com/watch?v=QUAItQmq-LU

--- README.md
+++ README.md
@@ -1,5 +1,23 @@
 # CW2
 Code and report for the second coursework for advanced web technologies
 
-Additional Libraries - RiotWatcher https://github.com/pseudonym117/Riot-Watcher
-to install  - pip install riotwatcher
+
+
+# README #
+
+
+
+**CW2**
+
+First redirect to CW2/src and check that you have all libraries installed, if not proceed to Step1, otherwise skip to Step2.
+
+**Step1**
+
+Assuming that you have Pip and VirtualEnv installed and your current folder is CW2/src, create a new virtualenv: $ virtualenv env
+
+Start your virtualenv and install the external libraries using requirements.txt, $ pip install -r requirements.txt
+
+**Step2**
+
+cd CW2/src/myapplication, first execute the command: execute the command python run.py
+
--- src/myapplication/run.py
+++ src/myapplication/run.py
@@ -1,42 +1,182 @@
 # -*- coding: utf-8 -*-
 import sys
-import json
+import os
+import collections
+import urllib, json
 import ConfigParser
 from flask import Flask, render_template, url_for, redirect, request, send_from_directory, \
     abort, \
     g, \
     flash, session
-from random import shuffle
-from riotwatcher import RiotWatcher, EUROPE_NORDIC_EAST
 from cassiopeia import riotapi
-from cassiopeia.type.core.common import LoadPolicy
-
+from cassiopeia.type.core.common import LoadPolicy, StatSummaryType
+from cassiopeia.type.api.exception import APIError
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config[
     'SQLALCHEMY_DATABASE_URI'] = 'sqlite:////home/tc/CW2/src/myapplication/var/league_of_legends.db'
+staticDataItems = "http://ddragon.leagueoflegends.com/cdn/6.22.1/data/en_US/item.json"
+response = urllib.urlopen(staticDataItems)
+data = json.loads(response.read())
+datapoints = data['data']
 
 db = SQLAlchemy(app)
 
 dblocation = 'var/sqlite3.db'
+# riotapi.set_region("EUNE")
+
+riotapi.print_calls(True)
+
+riotapi.set_api_key('23da5b32-c763-41ed-8d7a-f2b1023d8174')
+# riotapi.set_load_policy(LoadPolicy.lazy)
+riotapi.set_load_policy("eager")
 
 
-class summoners(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    summonername = db.Column(db.Unicode(50), unique=True)
-    summonerID = db.Column(db.Integer, unique=True)
+class Summoners(db.Model):
+    __tablename__ = 'summoners'
+    id = db.Column(db.Integer, primary_key=True, default=lambda: uuid.uuid4().hex)
+    summonerName = db.Column(db.Unicode(50), unique=True)
+    summonerLevel = db.Column(db.Integer)
+    summonerIcon = db.Column(db.Integer)
 
-    def __init__(self, id, summonername, summonerID):
+    # summonerID = db.Column(db.Integer, unique=True)
+
+    # def __init__(self, id, summonerName, summonerID):
+    def __init__(self, id, summonerName, summonerLevel, summonerIcon):
         self.id = id
-        self.summonername = summonername
-        self.summonerID = summonerID
+        self.summonerName = summonerName
+        self.summonerLevel = summonerLevel
+        self.summonerIcon = summonerIcon
+        # self.summonerID = summonerID
 
     def __repr__(self):
-        return '< ID %r with summonerName %s and summonerID %s>' % (
-            self.id, self.summonername, self.summonerID)
+        return ' %s' % self.summonerName
 
 
+class Items(db.Model):
+    __tablename__ = 'items'
+    id = db.Column(db.Integer, primary_key=True, default=lambda: uuid.uuid4().hex)
+    itemName = db.Column(db.Unicode(50))
+    itemImage = db.Column(db.Unicode)
+    itemDescription = db.Column(db.Unicode)
+    itemGold = db.Column(db.Integer)
+
+    # summonerID = db.Column(db.Integer, unique=True)
+
+    # def __init__(self, id, summonerName, summonerID):
+    def __init__(self, id, itemName, itemImage, itemDescription, itemGold):
+        self.id = id
+        self.itemName = itemName
+
+        # self.itemDesc = itemDesc
+        # self.itemText = itemText
+        self.itemImage = itemImage
+        self.itemDescription = itemDescription
+        self.itemGold = itemGold
+        # self.summonerID = summonerID
+
+    def __repr__(self):
+        return ' %s' % self.itemName
+
+
+class Matches(db.Model):
+    __tablename__ = 'matches'
+    id = db.Column(db.Integer, primary_key=True, default=lambda: uuid.uuid4().hex)
+    matchRegion = db.Column(db.Unicode(50))
+    matchDuration = db.Column(db.Integer)
+    matchParticipants = db.Column(db.Unicode(1000))
+    matchMode = db.Column(db.Unicode(50))
+
+    # summonerID = db.Column(db.Integer, unique=True)
+
+    # def __init__(self, id, summonerName, summonerID):
+    def __init__(self, id, matchRegion, matchDuration, matchParticipants, matchMode):
+        self.id = id
+        self.matchRegion = matchRegion
+        self.matchDuration = matchDuration
+        # self.itemDesc = itemDesc
+        # self.itemText = itemText
+        self.matchParticipants = matchParticipants
+        self.matchMode = matchMode
+        # self.summonerID = summonerID
+
+    def __repr__(self):
+        return ' %s' % self.id
+
+        # class Teams(db.Model):
+        #     __tablename__ = 'teams'
+        #     teamId = db.Column(db.Integer)
+        #     matchRegion = db.Column(db.Unicode(50))
+        #     matchDuration = db.Column(db.Integer)
+        #     matchId = db.Column(ForeignKey('matches.id'))
+        #     matchParticipants = db.Column(db.Unicode(1000))
+        #     matchMode = db.Column(db.Unicode(50))
+        #
+        #     matches = relationship("Matches")
+        #
+        #     __table_args__ = (
+        #         PrimaryKeyConstraint('teamId', 'matchId'),
+        #         ForeignKeyConstraint(
+        #             ['writer_id', 'magazine_id'],
+        #             ['writer.id', 'writer.magazine_id']
+        #         ),
+        #     )
+        #
+        #
+        #     # summonerID = db.Column(db.Integer, unique=True)
+        #
+        #     # def __init__(self, id, summonerName, summonerID):
+        #     def __init__(self, id, matchRegion, matchDuration, matchParticipants, matchMode):
+        #         self.id = id
+        #         self.matchRegion = matchRegion
+        #         self.matchDuration = matchDuration
+        #         # self.itemDesc = itemDesc
+        #         # self.itemText = itemText
+        #         self.matchParticipants = matchParticipants
+        #         self.matchMode = matchMode
+        #         # self.summonerID = summonerID
+        #
+        #     def __repr__(self):
+        #         return ' %s' % self.id
+        #
+
+
+
+        # class RunePages(db.Model):
+        #     id = db.Column(db.Integer, primary_key=True, default=lambda: uuid.uuid4().hex)
+        #     runePageName = db.Column(db.Unicode(50))
+        #     runePageCurrent = db.Column(db.BOOLEAN)
+        #     runePageRunes = db.Column(db.Unicode(500))
+        #     summoner_ID = db.Column(db.Integer, db.ForeignKey('summoner.id'))
+        #     foreign_keys =[Summoners.id]
+        # summonerID = db.Column(db.Integer, unique=True)
+
+        # def __init__(self, id, summonerName, summonerID):
+        # def __init__(self, id, runePageName, runePageCurrent, runePageRunes, summonerID):
+        #     self.id = id
+        #     self.runePageName = runePageName
+        #     self.runePageCurrent = runePageCurrent
+        #     self.runePageRunes = runePageRunes
+        #     self.summonerID = summonerID
+        #     # self.summonerID = summonerID
+        #
+        # def __repr__(self):
+        #     return ' %s' % self.runePageName
+
+
+db.drop_all()
+db.create_all()
+
+# enter all static item data in the database
+for item in datapoints.iteritems():
+
+    addItem = Items(id=item[0], itemName=item[1]['name'],
+                    itemDescription=item[1]['description'],
+                    itemImage=item[1]['image']['full'],
+                    itemGold=item[1]['gold']['base'])
+    db.session.add(addItem)
+    db.session.commit()
 
 
 def init(app):
@@ -44,138 +184,126 @@ def init(app):
     config_location = "etc/config.cfg"
     try:
         config.read(config_location)
-
         app.config['DEBUG'] = config.get("config", "debug")
         app.config['ip_address'] = config.get("config", "ip_address")
         app.config['port'] = config.get("config", "port")
         app.config['url'] = config.get("config", "url")
         app.secret_key = os.urandom(24)
-        app.permanent_session_lifetime = timedelta(seconds=60)
-
     except:
-        print ('Could not read config: '), config_location
-
-
-# Database functions
-
-# get access to database connection
-def get_db():
-    db = getattr(g, 'db', None)
-    if db is None:
-        db = sqlite3.connect(dblocation)
-        g.db = db
-        db.row_factory = sqlite3.Row
-    return db
-
-
-# @app.before_request
-# def before_request():
-#     g.user = None
-#     if 'user_id' in session:
-#         g.user = query_db('select * from user where user_id = ?',
-#                           [session['user_id']], one=True)
-
-# initialize schema
-def init_db():
-    with app.app_context():
-        db = get_db()
-        with app.open_resource('schema.sql', mode='r') as f:
-            db.cursor().executescript(f.read())
-        db.commit()
-
-
-# combines getting the cursor,executing and fetching tha results
-def query_db(query, args=(), one=False):
-    cur = get_db().execute(query, args)
-    rv = cur.fetchall()
-    return (rv[0] if rv else None) if one else rv
-
-
-@app.before_request
-def before_request():
-    g.user = None
-    if 'summonerName' in session:
-        g.user = query_db('select * from summoner where summonerName = ?',
-                          [session['summonerName']], one=True)
-
-
-# after each request
-@app.teardown_appcontext
-def close_db_connection(exception):
-    db = getattr(g, 'db', None)
-    if db is None:
-        db.close()
-
-
-# query for username
-def get_userid(username):
-    rv = query_db('select user_id from user where username = ?',
-                  [username], one=True)
-    return rv[0] if rv else None
-
-
-# query for summoner
-def get_summoner(summonerName):
-    rv = query_db('select summonerDBid from summoner where summonerName = ?',
-                  [summonerName], one=True)
-    return rv[0] if rv else None
-
-
-w = RiotWatcher('23da5b32-c763-41ed-8d7a-f2b1023d8174')
-
-# os.environ["DEV_KEY"] = "23da5b32-c763-41ed-8d7a-f2b1023d8174"
-riotapi.set_region("EUNE")
-
-riotapi.print_calls(True)
-
-riotapi.set_api_key('23da5b32-c763-41ed-8d7a-f2b1023d8174')
-riotapi.set_load_policy(LoadPolicy.lazy)
+        print ('Could not read  config: '), config_location
 
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
     err = None
+
     if request.method == 'POST':
         if not request.form['summonerName']:
-            err = 'Please provide us with your summoner name'
+            err = 'Please provide your summoner name'
         elif not request.form['region']:
-            err = 'Please write your E-mail address'
-        elif get_summoner(request.form['summonerName']) is not None:
-            err = 'The username is already taken'
+            err = 'Please set your region'
         else:
-            db = get_db()
-            db.execute('''INSERT INTO summonerDBid(
-              summonerName) VALUES (?)''',
-                       [request.form['summonerName']])
-            db.commit()
-            flash('Your summoner info has been stored')
             summonerName = request.form['summonerName']
-
-            return redirect(url_for('summoner', sumName=summonerName,
-                                    region=riotapi.set_region(request.form['region'])))
+            region = request.form['region']
+            return redirect(url_for('summoner', sumName=summonerName, region=region))
 
     return render_template('homepage.html', err=err)
 
+@app.route('/items')
+def items():
+    itemQuery = Items.query.all()
+    itemsD = {}
+    for item in itemQuery:
+        itemsD[item] = to_dict(item)
 
-@app.route('/summoner/<sumName>')
-def summoner(sumName):
-    # images_names = os.listdir('myapplication/static/images/planes')
-    # planeswalkerNames = planeswalker_dict.keys()
-    username = riotapi.get_summoner_by_name(sumName)
-    # rankedstats = riotapi.get_ranked_stats(sumName)
-    champions = riotapi.get_champions()
-    # championIds = riotapi.get_champions()
-    # mapping = {champion.id: champion.name for champion in championIds}
-    #
-    # runes = riotapi.get_rune_pages(sumName)
-    sumId = username.id
-    match1 = riotapi.get_match_list(username, 3)
-    championName = riotapi.get_champion_by_name(match1)
-    # match = riotapi.get_match(2034758953)
-    masteryStats = riotapi.get_champion_mastery_score(username)
-    return render_template('testhome.html', summoner=username, champions=champions,
-                           match=match1, championName=championName,
-                           masteryscore=masteryStats)
+    return render_template('items.html', item_info=itemsD)
+
+
+@app.route('/summoner/<region>/<sumName>')
+def summoner(region, sumName):
+    riotapi.set_region(region)
+    riotapi.set_load_policy(LoadPolicy.eager)
+    # check if the summoner is already in the database
+    exists = db.session.query(Summoners.id).filter(
+        Summoners.summonerName.ilike(sumName)).scalar() is not None
+    alreadyThere = ''
+    summonerData = []
+
+    if exists is False:
+        try:
+            # pull information about the summoner and store it in the db
+            username = riotapi.get_summoner_by_name(sumName)
+
+            addNewSumomner = Summoners(id=username.id, summonerName=username.name,
+                                       summonerLevel=username.level,
+                                       summonerIcon=username.profile_icon_id)
+            db.session.add(addNewSumomner)
+            db.session.commit()
+
+            # pull information about the rune pages of the summoner and store it in the db
+            rune_pages = riotapi.get_rune_pages(username)
+
+            # covert the results from the query into a dict with column names and value pairs
+            query = Summoners.query.filter_by(id=username.id).first()
+
+            summnersDic = to_dict(query)
+
+            match_list = username.match_list()
+            match = match_list[0].match()
+
+            return render_template('summonerPage.html', summoner=username,
+                                   alreadyThere=alreadyThere,
+                                   match_info=match,
+                                   summonerData=summnersDic,
+                                   rune_pages=rune_pages, region=region
+                                   )
+        except APIError as error:
+            if error.error_code in [404]:
+                return render_template('404.html', summoner=sumName)
+    else:
+
+        alreadyThere = 'They are there'
+
+        try:
+            username = riotapi.get_summoner_by_name(sumName)
+            rune_pages = riotapi.get_rune_pages(username)
+
+            query = Summoners.query.filter(
+                Summoners.summonerName.contains(sumName)).first()
+
+
+            # covert the results from the query into a dict with column names and value pairs
+            summnersDic = to_dict(query)
+
+            # gather the information about the last match of the summoner
+            match_list = username.match_list()
+            match = match_list[0].match()
+
+            return render_template('summonerPage.html', summoner=username,
+                                   alreadyThere=alreadyThere,
+                                   match_info=match,
+                                   summonerData=summnersDic,
+                                   rune_pages=rune_pages, region=region
+                                   )
+        except APIError as error:
+            if error.error_code in [404]:
+                return render_template('404.html', summoner=sumName)
+
+                # rune_pages = riotapi.get_rune_pages(username)
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('404.html', error=error, )
+
+# Convert flask sql alchemy queries to dictionaries
+def to_dict(model_instance, query_instance=None):
+    if hasattr(model_instance, '__table__'):
+        return {c.name: str(getattr(model_instance, c.name)) for c in
+                model_instance.__table__.columns}
+    else:
+        cols = query_instance.column_descriptions
+        return {cols[i]['name']: model_instance[i] for i in range(len(cols))}
 
 
 if __name__ == '__main__':
